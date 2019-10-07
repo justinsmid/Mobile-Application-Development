0c7e4dba533b09ccee5a5750d49cd938e71cb3f5
Justin Smid <justin.smid@hva.nl>
2019-10-07 14:10:03 +0200

    Added assignment 4.1

---
+++ Assignment 4.1(Shopping List)/app/src/main/java/com/example/shoppinglistapp/database/ShoppingListRoomDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.shoppinglistapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.shoppinglistapp.model.Product
+import com.example.shoppinglistapp.model.ProductDao
+
+@Database(entities = [Product::class], version = 1, exportSchema = false)
+abstract class ShoppingListRoomDatabase : RoomDatabase() {
+
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        private const val DATABASE_NAME = "SHOPPING_LIST_DATABASE"
+
+        @Volatile
+        private var shoppingListRoomDatabaseInstance: ShoppingListRoomDatabase? = null
+
+        fun getDatabase(context: Context): ShoppingListRoomDatabase? {
+            if (shoppingListRoomDatabaseInstance == null) {
+                synchronized(ShoppingListRoomDatabase::class.java) {
+                    if (shoppingListRoomDatabaseInstance == null) {
+                        shoppingListRoomDatabaseInstance =
+                            Room.databaseBuilder(context.applicationContext,
+                                ShoppingListRoomDatabase::class.java,
+                                DATABASE_NAME
+                            ).build()
+                    }
+                }
+            }
+            return shoppingListRoomDatabaseInstance
+        }
+    }
+
+}
